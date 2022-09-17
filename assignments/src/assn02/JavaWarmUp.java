466e8d06589d7ddf17079afc1ede97f744fe6d37
Reed Fu <rfugithub@outlook.com>
2022-09-17 12:41:44 -0400

    Assignment 2: Pt2 implement Transaction class

--- assignments/src/assn02/JavaWarmUp.java
+++ assignments/src/assn02/JavaWarmUp.java
@@ -2,6 +2,7 @@ package assn02;
 import java.util.Scanner;  // Import the Scanner class
 
 public class JavaWarmUp {
+    // 11/22/22, 12:23, food, 6.9, 69, 0.69, 69
     public static void main(String[] args) {
         // Construct a Java Scanner Obj
         Scanner scanner = new Scanner(System.in);
@@ -11,13 +12,13 @@ public class JavaWarmUp {
         int nTransaction = Integer.parseInt(scanner.nextLine());
 
         // Loop: Prompt and take in transactions
-        String[] Transactions = new String[nTransaction];
+        Transaction[] Transactions = new Transaction[nTransaction];
         for (int i = 0; i < nTransaction; i++) {
             String message = "Please enter transaction (" + (i+1) + "/" + nTransaction +
                     ") in the given format: MM/DD/YY, HH:MM, Category, Price, Quantity, Rating, Duration)";
             System.out.println(message);
-            Transactions[i] = scanner.nextLine();
+            Transactions[i] = new Transaction(scanner.nextLine());
         }
-        System.out.println(Transactions);
+        System.out.println(Transactions[0]);
     }
 }
---
+++ assignments/src/assn02/Transaction.java
@@ -0,0 +1,79 @@
+package assn02;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class Transaction {
+    private final LocalDateTime _dateTime; // date and time
+    private final String _category; // category of the products
+    private final float _price;  // unit price
+    private final int _quantity; // quantity of units
+    private final float _rating; // user rating: 0.0 - 5.0
+    private final int _duration; // duration of days for shipping
+
+    // constructor, string format: MM/DD/YY, HH:MM, Category, Price, Quantity, Rating, Duration
+    public Transaction(String inputString) {
+        String[] inputs = inputString.replaceAll("\\s", "").split(",");
+        if (inputs.length != 7) {
+            throw new IllegalArgumentException("Wrong format of transaction input!");
+        }
+        DateTimeFormatter inputPattern = DateTimeFormatter.ofPattern("MM/dd/yy,HH:mm");
+        _dateTime = LocalDateTime.parse(inputs[0] + ","+inputs[1], inputPattern);
+        _category = inputs[2];
+        _price = Float.parseFloat(inputs[3]);
+        _quantity = Integer.parseInt(inputs[4]);
+        _rating = Float.parseFloat(inputs[5]);
+        _duration = Integer.parseInt(inputs[6]);
+        if (_rating < 0 || _rating > 5) {
+            throw new IllegalArgumentException("Rating has to be between 0.0 to 5.0!");
+        }
+    }
+
+    public String get_date(){
+        DateTimeFormatter datePattern = DateTimeFormatter.ofPattern("MM/dd/yy");
+        return _dateTime.format(datePattern);
+    }
+
+    public String get_time(){
+        DateTimeFormatter timePattern = DateTimeFormatter.ofPattern("HH:mm");
+        return _dateTime.format(timePattern);
+    }
+
+    public String get_dateTime(){
+        DateTimeFormatter dateTimePattern = DateTimeFormatter.ofPattern("MM/dd/yy HH:mm");
+        return _dateTime.format(dateTimePattern);
+    }
+
+    public String get_category() {
+        return _category;
+    }
+
+    public float get_price() {
+        return _price;
+    }
+
+    public int get_quantity() {
+        return _quantity;
+    }
+
+    public float get_rating() {
+        return _rating;
+    }
+
+    public int get_duration() {
+        return _duration;
+    }
+
+    public String toString(){
+        String result = "";
+        result += "Date: " + get_date() + "\n";
+        result += "Time: " + get_time() + "\n";
+        result += "When: " + get_dateTime() + "\n";
+        result += "Category: " + get_category() + "\n";
+        result += "Price: " + get_price() + "\n";
+        result += "Quantity: " + get_quantity() + "\n";
+        result += "Rating: " + get_rating() + "\n";
+        result += "Duration: " + get_duration();
+        return result;
+    }
+}
