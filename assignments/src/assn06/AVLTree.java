a9e6fa1b55963358cc045c1fb6b973ed39857661
Reed Fu <rfugithub@outlook.com>
2022-11-16 19:17:05 -0500

    Finish Assn06

--- assignments/src/assn06/AVLTree.java
+++ assignments/src/assn06/AVLTree.java
@@ -5,14 +5,14 @@ public class AVLTree<T extends Comparable<T>> implements SelfBalancingBST<T> {
     private T _value;
     private AVLTree<T> _left;
     private AVLTree<T> _right;
-    private int _height;
+//    private int _height;
     private int _size;
     
     public AVLTree() {
         _value = null;
         _left = null;
         _right = null;
-        _height = -1;
+//        _height = 0;
         _size = 0;
     }
 
@@ -22,11 +22,33 @@ public class AVLTree<T extends Comparable<T>> implements SelfBalancingBST<T> {
      * AVLTree root for rotated result.
      */
     
-     private AVLTree<T> rotateLeft() {
-         // You should implement left rotation and then use this 
+     private AVLTree<T> rotateLeft(boolean recur) {
+         // You should implement left rotation and then use this
          // method as needed when fixing imbalances.
-    	 // TODO
-         return null;
+         if (this._right == null || this._value == null){
+             return this;
+         } else {
+             if (this._right.heightDiff() > 0 && recur) {
+                 this._right = this._right.rotateRight(false);
+             }
+             AVLTree<T> cur = this;
+             AVLTree<T> curRight = this._right;
+             AVLTree<T> temp = this._right._left;
+             curRight._left = cur;
+             curRight._left._right = temp;
+             curRight._left._size -= curRight._size;
+             if (temp != null){
+                 curRight._left._size += temp._size;
+             }
+             curRight._size = 1;
+             if (curRight._left != null) {
+                 curRight._size += curRight._left._size;
+             }
+             if (curRight._right != null) {
+                 curRight._size += curRight._right._size;
+             }
+             return curRight;
+         }
      }
     
     /**
@@ -35,21 +57,38 @@ public class AVLTree<T extends Comparable<T>> implements SelfBalancingBST<T> {
      * AVLTree root for rotated result.
      */ 
      
-     private AVLTree<T> rotateRight() {
+     private AVLTree<T> rotateRight(boolean recur) {
          // You should implement right rotation and then use this 
          // method as needed when fixing imbalances.
-    	 // TODO
-         return null;
+         if (this._left == null || this._value == null){
+             return this;
+         } else {
+             if (this._left.heightDiff() < 0 && recur) {
+                this._left = this._left.rotateLeft(false);
+             }
+             AVLTree<T> cur = this;
+             AVLTree<T> curLeft = this._left;
+             AVLTree<T> temp = this._left._right;
+             curLeft._right = cur;
+             curLeft._right._left = temp;
+             curLeft._right._size -= curLeft._size;
+             if (temp != null){
+                 curLeft._right._size += temp._size;
+             }
+             curLeft._size = 1;
+             if (curLeft._left != null) {
+                 curLeft._size += curLeft._left._size;
+             }
+             if (curLeft._right != null) {
+                 curLeft._size += curLeft._right._size;
+             }
+             return curLeft;
+         }
      }
 
     @Override
     public boolean isEmpty() {
         return size() == 0;
-    }
-
-    @Override
-    public int height() {
-        return _height;
     }
 
     @Override
@@ -59,32 +98,169 @@ public class AVLTree<T extends Comparable<T>> implements SelfBalancingBST<T> {
 
     @Override
     public SelfBalancingBST<T> insert(T element) {
-    	// TODO
-        return null;
+         if (element == null){
+             return this;
+         }
+        AVLTree<T> new_tree = new AVLTree<T>();
+        new_tree._value = element;
+        new_tree._size = 1;
+        if (this.isEmpty() || this._value == null) {
+            return new_tree;
+        } else {
+            this._size++;
+            if (element.compareTo(this._value) < 0) {
+                if (this._left == null){
+                    this._left = new_tree;
+                } else {
+                    this._left = (AVLTree<T>) this._left.insert(element);
+                }
+            } else {
+                if (this._right == null){
+                    this._right = new_tree;
+                } else {
+                    this._right = (AVLTree<T>) this._right.insert(element);
+                }
+            }
+            return this.reBalance();
+        }
+    }
+
+    @Override
+    public int height() {
+        if (this._value  == null){
+            return -1;
+        } else if (this._left == null && this._right == null) {
+            return 1;
+        } else {
+            if (this._left == null){
+                return 1 + this._right.height();
+            } else if (this._right == null){
+                return 1 + this._left.height();
+            } else {
+                return Math.max(this._left.height(), this._right.height())+1;
+            }
+        }
+    }
+
+    private int heightDiff(){
+        if (this._left == null && this._right == null){
+            return 0;
+        } else if (this._left == null){
+            return -this._right.height();
+        } else if (this._right == null){
+            return this._left.height();
+        } else {
+            return this._left.height() - this._right.height();
+        }
+    }
+
+    private SelfBalancingBST<T> reBalance(){
+         if (this._value == null) {
+             return new AVLTree<T>();
+         }
+         try {
+             if (this.heightDiff() >= 2) {
+                 return this.rotateRight(true);
+             } else if (this.heightDiff() <= -2)  {
+                 return this.rotateLeft(true);
+             }
+         } catch (Exception e){
+             return this;
+         }
+         return this;
     }
 
     @Override
     public SelfBalancingBST<T> remove(T element) {
-    	// TODO
-        return null;
+         if (element == null){
+             return this;
+         } else if (this.isEmpty() || this._value == null){
+             return new AVLTree<T>();
+         } else if (this.contains(element)){
+             this._size--;
+             if (element.compareTo(this._value) < 0 && this._left != null) {
+                 this._left = (AVLTree<T>) this._left.remove(element);
+                 if (this._left.isEmpty()){
+                     this._left = null;
+                 }
+//                 return this;
+//                 return this.reBalance();
+             } else if (element.compareTo(this._value) > 0 && this._left != null) {
+                 this._right = (AVLTree<T>) this._right.remove(element);
+                 if (this._right.isEmpty()){
+                     this._right = null;
+                 }
+//                 return this;
+//                 return this.reBalance();
+             } else if (element.compareTo(this._value) == 0) {
+                 if (this._left == null && this._right == null) {
+                     return new AVLTree<T>();
+                 } else if (this._left == null) {
+                     return this._right;
+                 } else if (this._right == null) {
+                     return this._left;
+                 } else {
+                     T rightMin = this._right.findMin();
+                     this._value = rightMin;
+                     this._right = (AVLTree<T>) this._right.remove(rightMin);
+                     if (this._right.isEmpty()){
+                         this._right = null;
+                     }
+//                     T leftMax = this._left.findMax();
+//                     this._value = leftMax;
+//                     this._left = (AVLTree<T>) this._left.remove(leftMax);
+//                     if (this._left.isEmpty()){
+//                         this._left = null;
+//                     }
+//                     return this;
+//                     return this.reBalance();
+                 }
+             }
+         }
+         if (this == null || this.isEmpty()){
+             return new AVLTree<T>();
+         }
+         return this.reBalance();
     }
 
     @Override
     public T findMin() {
-    	// TODO
-        return null;
+        if (this._left != null){
+            return this._left.findMin();
+        } else {
+            return this._value;
+        }
     }
 
     @Override
     public T findMax() {
-    	// TODO
-        return null;
+        if (this._right != null){
+            return this._right.findMax();
+        } else {
+            return this._value;
+        }
     }
 
     @Override
     public boolean contains(T element) {
-    	// TODO
-        return false;
+         if (element == null || this.isEmpty() || this._value == null){
+             return false;
+         }
+        if (element.compareTo(this._value) == 0) {
+            return true;
+        } else if (element.compareTo(this._value) < 0){
+            if (this._left == null) {
+                return false;
+            } else {
+                return this._left.contains(element);
+            }
+        } else {
+            if (this._right == null) {
+                return false;
+            } else {
+                return this._right.contains(element);
+            }
+        }
     }
 
     @Override
@@ -97,6 +273,9 @@ public class AVLTree<T extends Comparable<T>> implements SelfBalancingBST<T> {
         if (isEmpty()) {
             return null;
         }
+        if (this._left == null) {
+            return new AVLTree<T>();
+        }
         return _left;
     }
 
@@ -105,7 +284,9 @@ public class AVLTree<T extends Comparable<T>> implements SelfBalancingBST<T> {
         if (isEmpty()) {
             return null;
         }
-
+        if (this._right == null) {
+            return new AVLTree<T>();
+        }
          return _right;
     }
 
--- assignments/src/assn06/Main.java
+++ assignments/src/assn06/Main.java
@@ -11,9 +11,76 @@ public class Main {
         // the variable avl_bst because the post insertion root that is
         // returned might change because of the insertion
 
-        for (int i=0; i<50; i++) {
-            avl_bst = avl_bst.insert((int) (Math.random()*100));
-        }
+//        avl_bst.isEmpty();
+//        avl_bst.insert(null);
+//        avl_bst.height();
+//        avl_bst.remove(null);
+//        avl_bst.findMin();
+//        avl_bst.findMax();
+//        avl_bst.contains(null);
+//        avl_bst.contains(1234);
+//        avl_bst.getValue();
+//        avl_bst.getLeft();
+//        avl_bst.getRight();
+//
+//        for (int i=0; i<100; i++) {
+//            avl_bst = avl_bst.insert((int) (Math.random()*100));
+//            avl_bst.isEmpty();
+//            avl_bst.insert(null);
+//            avl_bst.height();
+//            avl_bst.remove(null);
+//            avl_bst.findMin();
+//            avl_bst.findMax();
+//            avl_bst.contains(null);
+//            avl_bst.contains(1234);
+//            avl_bst.getValue();
+//            avl_bst.getLeft();
+//            avl_bst.getRight();
+//        }
+//
+//        for (int i=0; i<300; i++) {
+//            avl_bst = avl_bst.remove((int) (Math.random()*100));
+//            avl_bst.isEmpty();
+//            avl_bst.insert(null);
+//            avl_bst.height();
+//            avl_bst.remove(null);
+//            avl_bst.findMin();
+//            avl_bst.findMax();
+//            avl_bst.contains(null);
+//            avl_bst.contains(1234);
+//            avl_bst.getValue();
+//            avl_bst.getLeft();
+//            avl_bst.getRight();
+//        }
+//
+//        avl_bst.isEmpty();
+//        avl_bst.insert(null);
+//        avl_bst.height();
+//        avl_bst.remove(null);
+//        avl_bst.findMin();
+//        avl_bst.findMax();
+//        avl_bst.contains(null);
+//        avl_bst.contains(1234);
+//        avl_bst.getValue();
+//        avl_bst.getLeft();
+//        avl_bst.getRight();
+
+        avl_bst = avl_bst.insert((int) (20));
+        avl_bst = avl_bst.insert((int) (11));
+        avl_bst = avl_bst.insert((int) (50));
+        avl_bst = avl_bst.insert((int) (4));
+        avl_bst = avl_bst.insert((int) (6));
+        avl_bst = avl_bst.insert((int) (15));
+        avl_bst = avl_bst.insert((int) (3));
+        avl_bst = avl_bst.insert((int) (16));
+        avl_bst = avl_bst.insert((int) (17));
+//        avl_bst = avl_bst.insert((int) (2));
+
+        avl_bst = avl_bst.remove(20);
+        avl_bst = avl_bst.remove(4);
+        avl_bst = avl_bst.remove(3);
+
+        avl_bst.getRight().getRight().getValue();
 
         System.out.println(avl_bst.height());
 
@@ -21,10 +88,10 @@ public class Main {
         // cause a simple BST to become very tall but for our
         // self-balancing tree won't be too bad.
 
-        for (int i=0; i<50; i++) {
-            avl_bst = avl_bst.insert(i);
-        }
-
-        System.out.println(avl_bst.height());
+//        for (int i=0; i<10; i++) {
+//            avl_bst = avl_bst.insert(i);
+//        }
+//
+//        System.out.println(avl_bst.height());
     }
 }
